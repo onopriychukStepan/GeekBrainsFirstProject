cdf7f8efbb179c4aa1bb4e2467b0be33d8f997a7
Stepan <stepan27013@mail,ru>
2022-02-20 16:19:27 +0300

    add Programm code

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace project{
+    class Program{
+        static void Main(){
+
+            string[] stroki = new string[] {"Hello", "2", "world", ":-)", "MADE_BY_STEPAN_ONOPRIYCHUK"};
+            string[] s = new string[8];
+
+        for (byte i = 0; i < stroki.Length; i++){
+            if(stroki[i].Length <= 3){
+                s[i] = stroki[i]+s[i];
+                System.Console.Write(s[i]);
+                System.Console.Write(' ');
+            }
+            }
+        }
+        }
+    
+    }
